5b509b449035bea6dc6c2c9967cf8b48900b3412
anokha777 <anokha777@gmail.com>
2018-08-26 20:46:25 +0530

    angular 6 directive directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-six-directive';
+  numbers = [1, 2, 3, 4, 5, 6, 7];
+  oddNumbers = [1, 3, 5, 7];
+  evenNumbers = [2, 4, 6];
+  onlyOdd = false;
+  value = "5";
 }
